488a964a68457166db28d0609932d2133c0b78d2
Michael Campbell <michael.campbell14@gmail.com>
2023-01-31 18:16:34 -0500

    CHORE spitting out the data for each day

--- src/components/DailyWeather.jsx
+++ src/components/DailyWeather.jsx
@@ -1,7 +1,42 @@
-const DailyWeather = () => {
+import SingleDayWeather from './SingleDayWeather';
+
+const DailyWeather = (props) => {
+  const dailyWeatherData = props.dailyWeatherData;
+  const dataUnits = props.dataUnits;
   return (
-    <div>
-      <p>daily weather section</p>
+    <div className='dailyWeatherBox'>
+      <p>
+        <b>
+          <u>DAILY WEATHER SECTION</u>
+        </b>
+      </p>
+      <div className='dailyWeatherFlexBox'>
+        {!dailyWeatherData ? (
+          <p style={{ textAlign: 'center', padding: 25 }}>
+            No Daily Weather Data
+          </p>
+        ) : (
+          dailyWeatherData.time.map((time, index) => (
+            <SingleDayWeather
+              key={time}
+              time={time}
+              apparent_temperature_max={
+                dailyWeatherData.apparent_temperature_max[index]
+              }
+              apparent_temperature_min={
+                dailyWeatherData.apparent_temperature_min[index]
+              }
+              precipitation_sum={dailyWeatherData.precipitation_sum[index]}
+              sunrise={dailyWeatherData.sunrise[index]}
+              sunset={dailyWeatherData.sunset[index]}
+              temperature_2m_max={dailyWeatherData.temperature_2m_max[index]}
+              temperature_2m_min={dailyWeatherData.temperature_2m_min[index]}
+              weathercode={dailyWeatherData.weathercode[index]}
+              dataUnits={dataUnits}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/SingleDayWeather.jsx
@@ -0,0 +1,61 @@
+const SingleDayWeather = (props) => {
+  const unixTimeTime = props.time;
+  const dateTime = new Date(unixTimeTime * 1000);
+
+  const unixTimeSunrise = props.sunrise;
+  const dateSunrise = new Date(unixTimeSunrise * 1000);
+
+  const unixTimeSunset = props.sunset;
+  const dateSunset = new Date(unixTimeSunset * 1000);
+
+  // TODO: for each day print out a readable date with day of the week, month name, number day of the month, year
+  // (thursday, Jan30, 2023)
+
+  return (
+    <div className='singleWeatherBox'>
+      <p>
+        <b>Time: </b>
+        {dateTime.toLocaleTimeString('en-US')}
+      </p>
+      <p>
+        <b>Apparent Temp Max: </b>
+        {props.apparent_temperature_max}
+        {props.dataUnits.apparent_temperature_max}
+      </p>
+      <p>
+        <b>Apparent Temp Min: </b>
+        {props.apparent_temperature_min}
+        {props.dataUnits.apparent_temperature_min}
+      </p>
+      <p>
+        <b>Precip Sum: </b>
+        {props.precipitation_sum}
+        {props.dataUnits.precipitation_sum}
+      </p>
+      <p>
+        <b>Sunrise: </b>
+        {dateSunrise.toLocaleTimeString('en-US')}
+      </p>
+      <p>
+        <b>Sunset: </b>
+        {dateSunset.toLocaleTimeString('en-US')}
+      </p>
+      <p>
+        <b>Temp 2m Max: </b>
+        {props.temperature_2m_max}
+        {props.dataUnits.temperature_2m_max}
+      </p>
+      <p>
+        <b>Temp 2m Min: </b>
+        {props.temperature_2m_min}
+        {props.dataUnits.temperature_2m_min}
+      </p>
+      <p>
+        <b>Weather Code: </b>
+        {props.weathercode}
+      </p>
+    </div>
+  );
+};
+
+export default SingleDayWeather;
