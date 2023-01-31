f31d685a6b72d024e997f1719e6bfd085d706924
Michael Campbell <michael.campbell14@gmail.com>
2023-01-31 18:15:52 -0500

    CHORE displaying the data for the current weather info

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,52 @@
-const CurrentWeather = () => {
+const CurrentWeather = (props) => {
+  const unixTime = props.currentWeatherData.time;
+  const date = new Date(unixTime * 1000);
+
   return (
-    <div>
-      <p>current weather section</p>
+    <div className='currentWeatherBox'>
+      <p>
+        <b>
+          <u>CURRENT WEATHER SECTION</u>
+        </b>
+      </p>
+      <p>
+        <b>Latitude: </b>
+        {props.lat ? props.lat : 'N/A'}
+      </p>
+      <p>
+        <b>Longitude: </b>
+        {props.long ? props.long : 'N/A'}
+      </p>
+      <p>
+        <b>Temp: </b>
+        {props.currentWeatherData.temperature
+          ? props.currentWeatherData.temperature
+          : 'N/A'}
+      </p>
+      <p>
+        <b>Time: </b>
+        {date.toLocaleTimeString('en-US')
+          ? date.toLocaleTimeString('en-US')
+          : 'N/A'}
+      </p>
+      <p>
+        <b>Weather Code: </b>
+        {props.currentWeatherData.weathercode
+          ? props.currentWeatherData.weathercode
+          : 'N/A'}
+      </p>
+      <p>
+        <b>Wind Direction: </b>
+        {props.currentWeatherData.winddirection
+          ? props.currentWeatherData.winddirection
+          : 'N/A'}
+      </p>
+      <p>
+        <b>Windspeed: </b>
+        {props.currentWeatherData.windspeed
+          ? props.currentWeatherData.windspeed
+          : 'N/A'}
+      </p>
     </div>
   );
 };
