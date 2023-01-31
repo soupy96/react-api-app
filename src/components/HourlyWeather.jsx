4b7a89a438ca7e076889ef6aaf41681468ecbb7f
Michael Campbell <michael.campbell14@gmail.com>
2023-01-31 18:16:58 -0500

    CHORE spitting out the data hourly

--- src/components/HourlyWeather.jsx
+++ src/components/HourlyWeather.jsx
@@ -1,7 +1,43 @@
-const HourlyWeather = () => {
+import SingleHourWeather from './SingleHourWeather';
+
+const HourlyWeather = (props) => {
+  const hourlyWeatherData = props.hourlyWeatherData;
+  const dataUnits = props.dataUnits;
   return (
-    <div>
-      <p>hourly weather section</p>
+    <div className='hourlyWeatherBox'>
+      <p>
+        <b>
+          <u>HOURLY WEATHER SECTION</u>
+        </b>
+      </p>
+      <div>
+        {!hourlyWeatherData ? (
+          <p style={{ textAlign: 'center', padding: 25 }}>
+            No Hourly Weather Data
+          </p>
+        ) : (
+          hourlyWeatherData.time.map((time, index) => (
+            <SingleHourWeather
+              key={time}
+              time={time}
+              apparent_temperature={
+                hourlyWeatherData.apparent_temperature[index]
+              }
+              cloudcover={hourlyWeatherData.cloudcover[index]}
+              precipitation={hourlyWeatherData.precipitation[index]}
+              relativehumidity_2m={hourlyWeatherData.relativehumidity_2m[index]}
+              surface_pressure={hourlyWeatherData.surface_pressure[index]}
+              temperature_2m={hourlyWeatherData.temperature_2m[index]}
+              visibility={hourlyWeatherData.visibility[index]}
+              weathercode={hourlyWeatherData.weathercode[index]}
+              winddirection_10m={hourlyWeatherData.winddirection_10m[index]}
+              windgusts_10m={hourlyWeatherData.windgusts_10m[index]}
+              windspeed_10m={hourlyWeatherData.windspeed_10m[index]}
+              dataUnits={dataUnits}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/SingleHourWeather.jsx
@@ -0,0 +1,72 @@
+const SingleHourWeather = (props) => {
+  const unixTime = props.time;
+  const dateTime = new Date(unixTime * 1000);
+
+  // TODO: for each hour print out a readable date with the hour, day of the week, month name, number day of the month, year
+  // (1:00pm, thursday, Jan30, 2023)
+
+  return (
+    <div className='hourlyWeatherFlexBox'>
+      <p>
+        <b>Time: </b>
+        {dateTime.toLocaleTimeString('en-US')}
+      </p>
+      <p>
+        <b>Apparent Temp: </b>
+        {props.apparent_temperature}
+        {props.dataUnits.apparent_temperature}
+      </p>
+      <p>
+        <b>Cloudcover: </b>
+        {props.cloudcover}
+        {props.dataUnits.cloudcover}
+      </p>
+      <p>
+        <b>Precip: </b>
+        {props.precipitation}
+        {props.dataUnits.precipitation}
+      </p>
+      <p>
+        <b>Relative Humidity: </b>
+        {props.relativehumidity_2m}
+        {props.dataUnits.relativehumidity_2m}
+      </p>
+      <p>
+        <b>Surface Pressure: </b>
+        {props.surface_pressure}
+        {props.dataUnits.surface_pressure}
+      </p>
+      <p>
+        <b>Temp 2m: </b>
+        {props.temperature_2m}
+        {props.dataUnits.temperature_2m}
+      </p>
+      <p>
+        <b>Visibility: </b>
+        {props.visibility / 1000}
+        {props.dataUnits.visibility}
+      </p>
+      <p>
+        <b>Weathercode: </b>
+        {props.weathercode}
+      </p>
+      <p>
+        <b>Wind Direction 10m: </b>
+        {props.winddirection_10m}
+        {props.dataUnits.winddirection_10m}
+      </p>
+      <p>
+        <b>Wind Gusts 10m: </b>
+        {props.windgusts_10m}
+        {props.dataUnits.windgusts_10m}
+      </p>
+      <p>
+        <b>Wind Speed 10m: </b>
+        {props.windspeed_10m}
+        {props.dataUnits.windspeed_10m}
+      </p>
+    </div>
+  );
+};
+
+export default SingleHourWeather;
